bafc69325fbc13948c30c39c438b9be263dd8e37
Rob Merrill <robgmerrill@gmail.com>
2019-07-20 16:36:12 -0700

    Add Pokegame component

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -17,6 +17,8 @@ class Pokedex extends Component {
         return (
             <div className="Pokedex">
                 <h1>Pokedex!</h1>
+                <p>Total exp: {this.props.exp}</p>
+                <p>{this.props.isWinner ? 'Winner!' : 'Loser'}</p>
                 <div className="Pokedex-cards">
                     {this.props.pokemon.map((p) => (
                         <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
